7607cf7d1a65cc8d60965e408d9b45281a946cb0
Aubrey T Dube <81259406+greytyler@users.noreply.github.com>
2025-06-14 23:51:53 +0200

    Update A-star.py

--- A-star.py
+++ A-star.py
@@ -8,7 +8,7 @@ pygame.display.set_caption("A* Path Finding Algorithm")
 
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
-BLUE = (0, 255, 0)
+BLUE = (0, 255, 0) 
 YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
