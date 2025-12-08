1797facb99197f119704140a71f7290997fb2044
Aubrey T Dube <81259406+greytyler@users.noreply.github.com>
2025-12-08 22:20:00 +0200

    Update A-star.py

--- A-star.py
+++ A-star.py
@@ -20,7 +20,7 @@ TURQUOISE = (64, 224, 208
 class Spot:
 	def __init__(self, row, col, width, total_rows):
 		self.row = row
-		self.col = col
+		self.col = coll
 		self.x = row * width
 		self.y = col * width
 		self.color = WHITE
